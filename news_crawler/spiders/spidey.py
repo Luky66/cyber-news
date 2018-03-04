85ac9cb8f43e7fa60eecda9b5f1b6fd98bf0066f
Lukas Bühler <lukasmc6@gmail.com>
2018-03-04 23:18:50 +0100

    forgot to pass index

--- news_crawler/spiders/spidey.py
+++ news_crawler/spiders/spidey.py
@@ -34,6 +34,7 @@ class SpideySpider(scrapy.Spider):
 
                 request = scrapy.Request(news.link, callback=self.parse_text)
                 request.meta['news'] = news
+                request.meta['index'] = index
                 return request
 
     def parse_text(self, response):
