d6017f3f3dcf476b3a67b09afa564687c76acf18
Lukas Bühler <lukasmc6@gmail.com>
2018-03-04 20:52:23 +0100

    Created cyber spider

---
+++ cyber-spider.py
@@ -0,0 +1,56 @@
+import scrapy
+
+class CyberSpider(scrapy.Spider):
+    name = 'cyber-spider'
+    start_urls = []
+
+    def start_requests(self):
+        for i, site in enumerate(sites):
+            self.start_urls.append( site.url )
+            yield self.make_requests_from_url(site.url, {'index': i})
+
+    def make_requests_from_url(self, url, meta):
+       return scrapy.Request(url, callback=self.parse, dont_filter=True, meta=meta)
+
+
+    def parse(self, response):
+        i = response.meta['index']
+
+        for feed in response.css(sites[i].feed_element_selector):
+            for entry in feed.css(sites[i].entry_element_selector):
+                yield {'title': entry.css(sites[i].title_element_selector+' ::text').extract_first()}
+
+
+        # for next_page in response.css('div.prev-post > a'):
+        #     yield response.follow(next_page, self.parse)
+
+    
+# custom stuff
+
+class Site:
+    """A crawlable news site."""
+
+    def __init__(self, 
+        url, 
+        feed_element_selector, 
+        entry_element_selector, 
+        title_element_selector, 
+        summary_element_selector, 
+        entry_link_selector):
+
+        self.url = url
+        self.feed_element_selector = feed_element_selector
+        self.entry_element_selector = entry_element_selector
+        self.title_element_selector = title_element_selector
+        self.summary_element_selector = summary_element_selector
+        self.entry_link_selector = entry_link_selector
+
+sites = [
+    Site(
+        "https://www.fifthdomain.com/dod/", 
+        "div.result-listing",
+        "article",
+        "h4",
+        "p.excerpt",
+        "a"
+    )]
