420d78e65fa88fa06d03eebac31185bf7c2ed303
Lukas Bühler <lukasmc6@gmail.com>
2018-03-04 23:08:43 +0100

    hopefully crawls text now

--- news_crawler/spiders/spidey.py
+++ news_crawler/spiders/spidey.py
@@ -12,23 +12,38 @@ class SpideySpider(scrapy.Spider):
             yield self.make_requests_from_url(site.url, {'index': i})
 
     def make_requests_from_url(self, url, meta):
-       return scrapy.Request(url, callback=self.parse, dont_filter=True, meta=meta)
+       return scrapy.Request(url, callback=self.parse_feed, dont_filter=True, meta=meta)
 
 
-    def parse(self, response):
+    def parse_feed(self, response):
         i = response.meta['index']
 
         for feed in response.css(sites[i].feed_element_selector):
             for entry in feed.css(sites[i].entry_element_selector):
-                yield {
-                    'title': entry.css(sites[i].title_element_selector+' ::text').extract_first(),
-                    'summary': entry.css(sites[i].summary_element_selector+' ::text').extract_first(),
-                    'link': entry.css(sites[i].entry_link_selector+' ::attr(href)').extract_first()
-                }
 
+                # Construct news element and pass it along
 
-        # for next_page in response.css('div.prev-post > a'):
-        #     yield response.follow(next_page, self.parse)
+                news = News(
+                    entry.css(sites[i].title_element_selector+' ::text').extract_first(),
+                    entry.css(sites[i].summary_element_selector+' ::text').extract_first(),
+                    entry.css(sites[i].entry_link_selector+' ::attr(href)').extract_first(),
+                    "",
+                    "",
+                    ""
+                )
+
+                request = scrapy.Request("http://www.example.com/some_page.html", callback=self.parse_text)
+                request.meta['news'] = news
+                return request
+
+    def parse_text(self, response):
+        news = response.meta['news']
+        i = response.meta['index']
+
+        paragraphs = entry.css(sites[i].text_paragraph_selector+' ::text').extract()
+        news.text = '\n'.join([str(p) for p in paragraphs])
+
+        yield news.makeJSON()
 
     
 # custom stuff
@@ -42,7 +57,8 @@ class Site:
         entry_element_selector, 
         title_element_selector, 
         summary_element_selector, 
-        entry_link_selector):
+        entry_link_selector,
+        text_paragraph_selector):
 
         self.url = url
         self.feed_element_selector = feed_element_selector
@@ -50,6 +66,39 @@ class Site:
         self.title_element_selector = title_element_selector
         self.summary_element_selector = summary_element_selector
         self.entry_link_selector = entry_link_selector
+        self.text_paragraph_selector
+
+
+
+class News:
+    """Defines all the things we want to store."""
+
+    title = ""
+    summary = ""
+    link = ""
+    text = ""
+    author = ""
+    datetime = ""
+
+
+    def __init__(self, title, summary, link, text, author, datetime)
+        self.title = title
+        self.summary = summary
+        self.link = link
+        self.text = text
+        self.author = author
+        self.datetime = datetime
+        
+    def makeJSON(self)
+        return {
+                    'title': ,
+                    'summary': 
+                    'link': 
+                }
+        
+
+
+# The sites
 
 sites = [
     Site(
@@ -58,5 +107,6 @@ sites = [
         "article",
         "h4",
         "p.excerpt",
-        "a"
+        "a",
+        "#article-content p"
     )]
